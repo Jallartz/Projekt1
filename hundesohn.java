817fc4e8b97dfa77487346591ba13c72b167a672
Jallartz <7qtsknjrvy@privaterelay.appleid.com>
2025-10-22 14:24:25 +0200

    commit versuch

---
+++ hundesohn.java
@@ -0,0 +1,9 @@
+class hundesohn {
+
+	public static void main(String[] args) {
+	System.out.println("Hallo Hundesohn!");
+	System.out.println("Wie geht es dir?");
+	System.out.println("Ich hoffe, du hast einen schönen Tag!");
+	}
+
+}
