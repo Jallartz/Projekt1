0329cd3bd8fe601dcadba5da2932b9da30572d25
Jallartz <7qtsknjrvy@privaterelay.appleid.com>
2025-10-23 11:57:36 +0200

    uebung1

--- hundesohn.java
+++ hundesohn.java
@@ -1,7 +1,7 @@
-class hundesohn {
+class joost {
 
 	public static void main(String[] args) {
-	System.out.println("Hallo Hundesohn!");
+	System.out.println("Hallo joost!");
 	System.out.println("Wie geht es dir?");
 	System.out.println("Ich hoffe, du hast einen schönen Tag!");
 	}
