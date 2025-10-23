8c084d5c2c36492f013a9133ee7b9d4b6c05e7c2
Jallartz <7qtsknjrvy@privaterelay.appleid.com>
2025-10-23 11:58:58 +0200

    commit

--- hundesohn.java
+++
@@ -1,9 +0,0 @@
-class joost {
-
-	public static void main(String[] args) {
-	System.out.println("Hallo joost!");
-	System.out.println("Wie geht es dir?");
-	System.out.println("Ich hoffe, du hast einen schönen Tag!");
-	}
-
-}
---
+++ joost.java
@@ -0,0 +1,7 @@
+class joost {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
